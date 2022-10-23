dc98cab9647be960565ba1c2b064558c69905145
Liz Myers <liz@myersdesign.com>
2022-10-23 16:05:17 -0400

    Loading local json
    working

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,26 +1,14 @@
 import { useEffect, useState } from 'react'
 import Wordle from './components/Wordle'
+import data from './data/db.json'
 
 function App() {
   const [solution, setSolution] = useState(null)
-  
+ 
   useEffect(() => {
-    fetch('http://localhost:3000/data/db.json/solutions'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    
-    }
-    )
-      .then(res => res.json())
-      .then(json => {
-        // random int between 0 & 14
-      
-        const randomSolution = json[Math.floor(Math.random()*json.length)]
-        setSolution(randomSolution.word)
-      })
+  const words = data.solutions
+  const randomSolution = words[Math.floor(Math.random()*words.length)]
+      setSolution(randomSolution.word)
   }, [setSolution])
 
   return (
